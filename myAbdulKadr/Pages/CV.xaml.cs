c4f95e8f9ff0d187c77b366e5bf58e4dfe767f4e
telman <telman@TELMAN-PC>
2020-02-02 22:30:20 +0400

    filter by position Structure

--- myAbdulKadr/Pages/CV.xaml.cs
+++ myAbdulKadr/Pages/CV.xaml.cs
@@ -2,6 +2,7 @@
 using PersonMotion.Common;
 using PersonMotion.Model;
 using PersonMotion.WF;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -15,42 +16,53 @@ namespace myAbdulKadr.Pages
     /// <summary>
     /// Interaction logic for CV.xaml
     /// </summary>
+    /// 
+    public class SearchFieds
+    {
+        public int? OrgID { get; set; }
+        public int? DeptID { get; set; }
+        public int? SecID { get; set; }
+    }
+
     public partial class CV : UserControl
     {
         private peopleEntities dbContext = DBContextResolver.Instance;
 
-
+        private static ControlData cd = new ControlData();
+        SearchFieds sf = new SearchFieds();
 
         public CV()
         {
             InitializeComponent();
+            GlobalCache.showdialog = true;
+
+            cmbOrganization.ItemsSource = cd.GetOrganizationList();
+            cmbOrganization.DisplayMemberPath = "organizationName";
+            cmbOrganization.SelectedValuePath = "ID";
 
             perList.DataContext = new EmployeeViewModel();
         }
 
-        private ObservableCollection<EMP_LIST> GetData(string name, string surname, string midname)
+        private ObservableCollection<EMP_LIST> GetData(string name, string surname, string midname, int? orgID, int? deptID, int? sectID)
         {
             List<EMP_LIST> employeeList = null;
-            if (string.IsNullOrEmpty(name) && string.IsNullOrEmpty(surname) && string.IsNullOrEmpty(midname))
+            if (string.IsNullOrEmpty(name) && string.IsNullOrEmpty(surname) && string.IsNullOrEmpty(midname) && !orgID.HasValue)
             {
                 employeeList = new EmployeeViewModel().Employees.ToList();
             }
             else
             {
-                employeeList = new EmployeeViewModel().Employees.Where(m => m.name.Contains(name) && m.surname.Contains(surname) && m.secondname.Contains(midname)).ToList();
-
+                employeeList = new EmployeeViewModel().Employees.Where(m => m.name.Contains(name) && m.surname.Contains(surname) && m.secondname.Contains(midname) && (sf.OrgID == null || m.orgID == sf.OrgID) && (sf.DeptID == null || m.deptID == sf.DeptID) && (sf.SecID == null || m.sectID == sf.SecID)).ToList();
             }
-            //new { m.ID, m.name, m.surname, m.secondname, m.birthdate, m.birthplace });
+
             return new ObservableCollection<EMP_LIST>(employeeList);
-            //return ObservableCollection<d;
         }
 
 
-        
+
         private void SearchButton_Click(object sender, RoutedEventArgs e)
         {
-
-            perList.ItemsSource = GetData(pName.Text, pSurname.Text, pMidName.Text);
+            perList.ItemsSource = GetData(pName.Text, pSurname.Text, pMidName.Text, sf.OrgID, sf.DeptID, sf.OrgID);
         }
 
         private void PerList_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -62,17 +74,10 @@ namespace myAbdulKadr.Pages
 
                 if (selectedEmp.ID > 0)
                 {
-
-
-                   
                     HRCard hc = new HRCard();
-                 
-                    hc.personID =  selectedEmp.ID.ToString();
-                   
+                    hc.personID = selectedEmp.ID.ToString();
                     hc.ShowDialog();
                 }
-
-
             }
         }
 
@@ -88,15 +93,56 @@ namespace myAbdulKadr.Pages
 
         public void OnNavigatedTo(NavigationEventArgs e)
         {
-       
-            
 
-       
+
+
+
         }
 
         public void OnNavigatingFrom(NavigatingCancelEventArgs e)
         {
 
+        }
+
+        private void CmbOrganization_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            sf.OrgID = Convert.ToInt32(cmbOrganization.SelectedValue);
+
+
+            cmbDepartment.ItemsSource = sf.OrgID.HasValue ? cd.GetDepartmentList(sf.OrgID.Value) : null;
+            cmbDepartment.DisplayMemberPath = "departmentName";
+            cmbDepartment.SelectedValuePath = "ID";
+
+
+        }
+
+        private void CmbDepartment_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            sf.DeptID = Convert.ToInt32(cmbDepartment.SelectedValue);
+
+            cmbSection.ItemsSource = sf.DeptID.HasValue ? cd.GetSectionList(sf.DeptID.Value) : null;
+            cmbSection.DisplayMemberPath = "sectionName";
+            cmbSection.SelectedValuePath = "ID";
+
+        }
+
+        private void CmbSection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            sf.SecID = Convert.ToInt32(cmbSection.SelectedValue);
+        }
+
+        private void ClearFilter_Click(object sender, RoutedEventArgs e)
+        {
+            pName.Text = string.Empty;
+            pSurname.Text = string.Empty;
+            pMidName.Text = string.Empty;
+            cmbOrganization.SelectedIndex = -1;
+            cmbDepartment.SelectedIndex = -1;
+            cmbSection.SelectedIndex = -1;
+            sf.OrgID = null;
+            sf.DeptID = null;
+            sf.SecID = null;
         }
     }
 }
--- myAbdulKadr/Pages/Home.xaml.cs
+++ myAbdulKadr/Pages/Home.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
+
 namespace hydrogen.Pages
 {
     /// <summary>
@@ -20,9 +21,9 @@ namespace hydrogen.Pages
     /// 
     public class SearchFieds
     {
-        public int? orgID { get; set; }
-        public int? deptID { get; set; }
-        public int? secID { get; set; }
+        public int? OrgID { get; set; }
+        public int? DeptID { get; set; }
+        public int? SecID { get; set; }
     }
 
     public partial class Home : UserControl, IContent
@@ -39,24 +40,7 @@ namespace hydrogen.Pages
             cmbOrganization.ItemsSource = cd.GetOrganizationList();
             cmbOrganization.DisplayMemberPath = "organizationName";
             cmbOrganization.SelectedValuePath = "ID";
-            // cmbOrganization.SelectedValue = positionRecord.orgID;
 
-            //var selectedOrgID = Convert.ToInt32(cmbOrganization.SelectedValue);
-            // cmbDepartment.ItemsSource = positionRecord.orgID.HasValue ? cd.GetDepartmentList(positionRecord.orgID.Value) : null;
-            // cmbDepartment.DisplayMemberPath = "departmentName";
-            // cmbDepartment.SelectedValuePath = "ID";
-            //// cmbDepartment.SelectedValue = positionRecord.deptID;
-
-            // //var selectedDeptID = Convert.ToInt32(cmbDepartment.SelectedValue);
-            // cmbSection.ItemsSource = positionRecord.deptID.HasValue ? cd.GetSectionList(positionRecord.deptID.Value) : null;
-            // cmbSection.DisplayMemberPath = "sectionName";
-            // cmbSection.SelectedValuePath = "ID";
-            //// cmbSection.SelectedValue = positionRecord.sectID;
-
-
-
-
-            //GetData(string.Empty, string.Empty, string.Empty);
         }
 
         private ObservableCollection<EMP_LIST> GetData(string name, string surname, string midname, int? orgID, int? deptID, int? sectID)
@@ -68,14 +52,13 @@ namespace hydrogen.Pages
             }
             else
             {
-                employeeList = new EmployeeViewModel().Employees.Where(m => m.name.Contains(name) && m.surname.Contains(surname) && m.secondname.Contains(midname)&&m.orgID==sf.orgID&&m.deptID==sf.deptID&&m.sectID==sf.secID).ToList();
-                //where m.name.Contains(name) && m.surname.Contains(surname) && m.secondname.Contains(midname)
-                //select m).ToList();
-
+                employeeList = new EmployeeViewModel().Employees.Where(m => m.name.Contains(name) && m.surname.Contains(surname) && m.secondname.Contains(midname)&&(sf.OrgID==null||m.orgID==sf.OrgID)&&(sf.DeptID==null||m.deptID==sf.DeptID)&&(sf.SecID== null||m.sectID==sf.SecID)).ToList();
             }
-            //new { m.ID, m.name, m.surname, m.secondname, m.birthdate, m.birthplace });
+            int totalValue = employeeList.Count();
+            int totalNotExists = employeeList.Where(d=>d.isfired==true).Count();
+            int totalExists = totalValue- totalNotExists;
             return new ObservableCollection<EMP_LIST>(employeeList);
-            //return ObservableCollection<d;
+           
         }
 
 
@@ -87,8 +70,7 @@ namespace hydrogen.Pages
 
         private void SearchButton_Click(object sender, RoutedEventArgs e)
         {
-            
-            perList.ItemsSource = GetData(pName.Text, pSurname.Text, pMidName.Text,  sf.orgID, sf.deptID, sf.orgID);
+             perList.ItemsSource = GetData(pName.Text, pSurname.Text, pMidName.Text,  sf.OrgID, sf.DeptID, sf.OrgID);
         }
 
         private const string mainWindowHeader = "Person motion";
@@ -102,17 +84,12 @@ namespace hydrogen.Pages
 
                 if (selectedEmp.ID > 0)
                 {
-
-
                     string url = "../PersonalDataPages/PersonDataEditor.xaml#" + selectedEmp.ID.ToString();
                     Window mainWindow = Application.Current.MainWindow;
                     personFullName = dbContext.employee.Where(s => s.ID == selectedEmp.ID).Select(s => s.name + " " + s.surname + " " + s.secondname).SingleOrDefault();
                     mainWindow.Title = mainWindowHeader + " " + personFullName;
                     NavigationCommands.GoToPage.Execute(url, this);
-
                 }
-
-
             }
         }
 
@@ -128,17 +105,7 @@ namespace hydrogen.Pages
 
         public void OnNavigatedTo(NavigationEventArgs e)
         {
-            // perList.ItemsSource = null;
-            // perList.ItemsSource = GetData(string.Empty, string.Empty, string.Empty);
-            //perList.Items.Refresh();
-            // var d = new EmployeeViewModel();
             perList.DataContext = new EmployeeViewModel();
-
-            //perList.ItemsSource = null;
-
-            //         perList.ItemsSource = new EmployeeViewModel().Employees;//Preferably do this somewhere else, not in the add method.
-            //perList.Items.Refresh();
-
         }
 
         public void OnNavigatingFrom(NavigatingCancelEventArgs e)
@@ -149,21 +116,17 @@ namespace hydrogen.Pages
         private void CmbOrganization_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             
-            sf.orgID = Convert.ToInt32(cmbOrganization.SelectedValue);
-
-
-            cmbDepartment.ItemsSource = sf.orgID.HasValue ? cd.GetDepartmentList(sf.orgID.Value) : null;
+            sf.OrgID = Convert.ToInt32(cmbOrganization.SelectedValue);
+            cmbDepartment.ItemsSource = sf.OrgID.HasValue ? cd.GetDepartmentList(sf.OrgID.Value) : null;
             cmbDepartment.DisplayMemberPath = "departmentName";
             cmbDepartment.SelectedValuePath = "ID";
-           
-
         }
 
         private void CmbDepartment_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            sf.deptID= Convert.ToInt32(cmbDepartment.SelectedValue);
+            sf.DeptID= Convert.ToInt32(cmbDepartment.SelectedValue);
 
-            cmbSection.ItemsSource = sf.deptID.HasValue ? cd.GetSectionList(sf.deptID.Value) : null;
+            cmbSection.ItemsSource = sf.DeptID.HasValue ? cd.GetSectionList(sf.DeptID.Value) : null;
             cmbSection.DisplayMemberPath = "sectionName";
             cmbSection.SelectedValuePath = "ID";
             
@@ -171,7 +134,20 @@ namespace hydrogen.Pages
 
         private void CmbSection_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            sf.secID = Convert.ToInt32(cmbSection.SelectedValue);
+            sf.SecID = Convert.ToInt32(cmbSection.SelectedValue);
+        }
+
+        private void ClearFilter_Click(object sender, RoutedEventArgs e)
+        {
+            pName.Text = string.Empty;
+            pSurname.Text = string.Empty;
+            pMidName.Text = string.Empty;
+            cmbOrganization.SelectedIndex = -1;
+            cmbDepartment.SelectedIndex = -1;
+            cmbSection.SelectedIndex = -1;
+            sf.OrgID = null;
+            sf.DeptID = null;
+            sf.SecID = null;
         }
     }
 }
